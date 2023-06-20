9e432efec62e5e86937234b04b4765b56d868249
Vlad Onis <vladonis.biz+opensource@gmail.com>
2023-06-20 09:18:11 +0300

    Extract a list of links and titles from a page * Parse the page in search for a given string

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,6 +1,6 @@
 use scraper::{Html, Selector};
 use thiserror::Error;
-use tracing::error;
+use tracing::{error, info, warn};
 
 pub const ISSUE_BACK_BONE_THIS: &str = "this-week-in-rust-";
 pub const ISSUE_BACK_BONE_LAST: &str = "last-week-in-rust-";
@@ -15,6 +15,33 @@ pub enum CrawlerError {
     SelectorParsing,
 }
 
+// todo: Return type could be a nicer structure instead of a tuple of string, string
+// The first returned String in the tuple is the link, the other is the title
+//
+// Structure of a link:
+// "<a href=\"https://this-week-in-rust.org/blog/2013/08/10/this-week-in-rust-10/\">This Week in Rust 10</a>"
+fn extract_link_and_title(link: String) -> (String, String) {
+    // This function removes the html tags and quotes in the link
+    // splitting it into the link part and the title part
+    let link = link.replace("<a href=\"", "");
+
+    // Abomination that removes all the html after the end of the link
+    // It assumes that the html link ends with "> that is why it splits for the quote
+    let elements: Vec<&str> = link.split('"').collect();
+    let currated_link = elements.first().unwrap_or(&"").to_string();
+
+    // Abomination that removes all the html bits in the title part
+    // It assumes that the title still has ">" and "</a>"
+    // It uses replace because in case the characters are not found the title is unaltered
+    let title = elements
+        .last()
+        .unwrap_or(&"")
+        .replace("</a>", "")
+        .replace(">", "");
+
+    (currated_link, title)
+}
+
 /// This function makes a few assumptions about the underlying Html:
 /// 1. Each link starts with  <a href=\" (including the quotes)
 /// 2. Each link is valid -> this will be fixed with lychee
@@ -22,12 +49,10 @@ pub enum CrawlerError {
 ///
 /// Valid link example: <a href="https://this-week-in-rust.org/blog/2020/07/14/this-week-in-rust-347/">This Week in Rust 347</a>
 /// Currated link collected by this function: https://this-week-in-rust.org/blog/2019/02/26/this-week-in-rust-275/
-pub async fn get_latest_issue_index() -> Result<Vec<String>, CrawlerError> {
+pub async fn get_latest_issue_index() -> Result<Vec<(String, String)>, CrawlerError> {
     // todo:
     // 1. verify your links with lychee: https://github.com/lycheeverse/lychee
     // 2. Do not return Vec<String> but a Link type that is verified when constructed
-    // 4. Logging.
-    //
 
     let client = reqwest::Client::new();
     let origin_url = "https://this-week-in-rust.org/blog/archives/index.html";
@@ -38,7 +63,7 @@ pub async fn get_latest_issue_index() -> Result<Vec<String>, CrawlerError> {
     // Todo: Do not swallow the error here
     let selector = Selector::parse("a").map_err(|_| CrawlerError::SelectorParsing)?;
 
-    let links: Vec<String> = document
+    let links_and_titles: Vec<(String, String)> = document
         .select(&selector)
         .map(|element| element.html())
         .filter(|element| {
@@ -46,24 +71,40 @@ pub async fn get_latest_issue_index() -> Result<Vec<String>, CrawlerError> {
                 || element.contains(ISSUE_BACK_BONE_LAST)
                 || element.contains(ISSUE_BACK_BONE_THESE)
         })
-        .map(|element| {
-            // trim beginnig tag
-            let currated_link = element.replace("<a href=\"", "");
-
-            // Abomination that removes all the html after the end of the link
-            // It assumes that the html link ends with "> that is why it splits for the quote
-            let currated_link = currated_link
-                .split('"')
-                .collect::<Vec<&str>>()
-                .first()
-                .unwrap_or(&"")
-                .to_owned()
-                .to_owned();
-            currated_link
-        })
+        // .for_each(|element| info!("{element:?}"))
+        .map(|element| extract_link_and_title(element))
         .collect();
 
-    Ok(links)
+    Ok(links_and_titles)
+}
+
+pub async fn parse_page() {
+    let client = reqwest::Client::new();
+    let origin_url = "https://this-week-in-rust.org/blog/2022/04/27/this-week-in-rust-440/";
+    let response = client
+        .get(origin_url)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    let document = Html::parse_document(&response);
+
+    // Todo: Do not swallow the error here
+    let selector = Selector::parse("a")
+        .map_err(|_| CrawlerError::SelectorParsing)
+        .unwrap();
+
+    let links: Vec<(String, String)> = document
+        .select(&selector)
+        .map(|element| element.html())
+        .map(|element| extract_link_and_title(element))
+        .filter(|(_, title)| title.contains("embedded audio")) // just an example from 440th issue
+        .collect();
+
+    warn!("{links:?}");
 }
 
 #[allow(dead_code)]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 pub mod crawler;
 pub mod logger;
 
-use crawler::get_latest_issue_index;
+use crawler::{get_latest_issue_index, parse_page};
 
 use tracing::{error, info};
 
@@ -9,14 +9,18 @@ use tracing::{error, info};
 async fn main() {
     logger::set_tracing();
 
-    let links = get_latest_issue_index().await;
-    match links {
-        Ok(links) => {
-            info!("Obtained {} this week in rust issue links", links.len());
+    let links_and_titles = get_latest_issue_index().await;
+    match links_and_titles {
+        Ok(links_and_titles) => {
+            links_and_titles
+                .into_iter()
+                .for_each(|(link, title)| info!("Obtained link: {} \t and title: {}", link, title));
         }
         Err(e) => {
             error!("Error occured while getting the links from twir: {e:?}");
             std::process::exit(-1);
         }
     }
+
+    parse_page().await;
 }
